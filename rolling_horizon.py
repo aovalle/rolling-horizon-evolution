86153ed36a3d50c43895e617f2a46dd94f8a469e
Alvaro Ovalle <aovalle@zoho.com>
2020-04-30 11:44:09 +0100

    Updated mutated function
    mutate() now generates all action sequences simultaneously instead of generating one by one during sequence evaluation eval_seq().

--- rolling_horizon.py
+++ rolling_horizon.py
@@ -26,20 +26,19 @@ class RollingHorizon():
         else: # if we want to use shift buffer
             rollout = self.shift_buffer(np.copy(self.curr_rollout))
 
+        # Mutate rollout
+        rollouts = self.mutate(rollout)
+
         # Evaluate rollouts and get the best one
-        self.curr_rollout = self.eval_seq(cloned_state, rollout, simulator)
+        self.curr_rollout = self.eval_seq(cloned_state, rollouts, simulator)
         action = self.curr_rollout[0]
         return action, self.curr_rollout
 
-    def eval_seq(self, init_state, rollout, simulator):
+    def eval_seq(self, init_state, rollouts, simulator):
         cloned_env = copy.deepcopy(simulator)
         highest_reward = float("-inf")
         best_rollout = None
-        for candidate in range(self.candidates):
-            if candidate == 0:
-                mutated_rollout = rollout
-            else:
-                mutated_rollout = self.mutate(np.copy(rollout))
+        for rollout in rollouts:
 
             # (Re)init simulated environment
             env = copy.deepcopy(cloned_env)
@@ -47,26 +46,24 @@ class RollingHorizon():
 
             rollout_reward = 0
             for step in range(self.horizon):
-                action = mutated_rollout[step] #1
+                action = rollout[step] #1
                 state, reward, done, _ = env.step(action)
                 rollout_reward += reward
                 if done:
                     break
             if rollout_reward > highest_reward:
-                best_rollout = np.copy(mutated_rollout)
+                best_rollout = np.copy(rollout)
                 highest_reward = rollout_reward
         return best_rollout
 
     def mutate(self, rollout):
-        # Determine how many elements to sample from the rollout
-        n = np.random.binomial(len(rollout), self.mutation_rate)
-        # Generate indices of the sequence to be mutated
-        idx = d = np.unique(np.random.randint(0, len(rollout), size=n))
-        # Sample new actions
-        new_actions = np.random.randint(self.num_actions, size=len(d))
-        # Substitute values and obtain mutated sequence
-        rollout[idx] = new_actions
-        return rollout
+        # Clone sequence
+        rollouts = np.tile(rollout, (self.candidates, 1))
+        # Generate indices to mutate
+        idx = np.random.rand(*rollouts.shape) < self.mutation_rate
+        # Generate new actions and place them accordingly
+        rollouts[idx] = np.random.randint(self.num_actions, size=len(idx[idx is True]))
+        return rollouts
 
     def shift_buffer(self, rollout):
         # append new random action at the end
